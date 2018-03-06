7b9e4bb8e3f909935487ebe0c53ed870254d2cbb
Germain Loret <germain.loret@gmail.com>
2018-03-06 10:41:42 +0100

    create crud for restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # # read all restaurants
+  # get "/restaurants", to: "restaurants#index"
+  # # read one restaurant
+  # get "/restaurants/:id", to: "restaurants#show"
+  # # create one restaurant
+  # get "/restaurants/new", to: "restaurants#new"
+  # post "/restaurants", to: "restaurants#create"
+  # # update one restaurant
+  # get "/restaurants/:id/edit", to: "restaurants#edit"
+  # patch "/restaurants/:id", to: "restaurants#update"
+  # # delete one restaurant
+  # delete "/restaurants/:id", to: "restaurants#destroy"
+
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
